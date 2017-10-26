0afd83a6d0e29849fe675c2296828d8dcfd37ab8
Jannik Neumann <jjmn@live.de>
2017-10-26 12:59:09 +0200

    notiz

--- ObjekteGeldautomat/Program.cs
+++ ObjekteGeldautomat/Program.cs
@@ -29,13 +29,14 @@ namespace ObjekteGeldautomat
     {
         static void Main(string[] args)
         {
-            Konto konto1 = new Konto();
-            Konto konto2 = new Konto();
-            Konto konto3 = new Konto();
+            Konto konto1 = new Konto();     // Geht das nicht auch in einem Array?
+            Konto konto2 = new Konto();     // Geht das nicht auch in einem Array?
+            Konto konto3 = new Konto();     // Geht das nicht auch in einem Array?
             konto1.buchung(100);
             konto2.buchung(2000);
             konto3.buchung(50000);
             Console.WriteLine("Kontostand von Konto 1: {0}\nKontostand von Konto 2: {1}\nKontostand von Konto 3: {2}", konto1.getKtoStand(),konto2.getKtoStand(),konto3.getKtoStand());
+            Console.WriteLine();
             Console.ReadLine();
         }
     }
