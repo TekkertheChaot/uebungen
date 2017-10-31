0bcbc7c01461b8da6d14953050349e24d2e42cf0
Jannik Neumann <jjmn@live.de>
2017-10-31 21:38:22 +0100

    AutoundReifen: Kommentare hinzugefügt

--- AutoundReifen/Program.cs
+++ AutoundReifen/Program.cs
@@ -8,16 +8,15 @@ namespace AutoundReifen
 {
     class Auto
     {
-        private Reifen[] arrReifen = new Reifen[4];
-
-        public int checkReifenDruck(int Reifennummer)       //Reifendruck abfragen
+        private Reifen[] arrReifen = new Reifen[4];         //Reifen-Arraay Anlegen
+        public int checkReifenDruck(int Reifennummer)       //Reifendruck abfragen, Reifennummer Hinterlegen
         {
-            return arrReifen[Reifennummer].getDruck();
+            return arrReifen[Reifennummer].getDruck();      //Ruft Methode aus der Klasse 'Reifen' ab
         }
 
         public bool checkReifen(int Reifennummer)           //Reifen existent?
         {
-            if (arrReifen[Reifennummer] != null)
+            if (arrReifen[Reifennummer] != null)            //Ist die Stelle <Reifennummer> im Array initiallisiert worden? (möglich durch das Aufrufen der 'addReifen' Methode)
             {
                 return true;
             }
@@ -54,16 +53,18 @@ namespace AutoundReifen
         }
     }
 
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            int anzahlReifen = 0;
-            Auto MyAuto = new Auto();
-            MyAuto.addReifen(1, 21);
+            int anzahlReifen = 0;                   //Speichert später die Anzahl der hinzugefügten Reifen
+            Auto MyAuto = new Auto();               //Auto wird erstellt
+            MyAuto.addReifen(1, 21);                //Reifen werden angebracht (Es wird im 'arrReifen' die Stelle im Index 1 initiallisiert und der Reifen bekommt Druck (21))
             MyAuto.addReifen(3, 25);
 
-            for (int i = 0; i < 4;i++ )
+            for (int i = 0; i < 4;i++ )             //Zählt die angebrachten (initialisierten) Reifen
             {
                 if (MyAuto.checkReifen(i) == true)
                 {
