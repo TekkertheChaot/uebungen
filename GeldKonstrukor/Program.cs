240d0834a421978eb0db4d2f01dbb07f4c2226a0
Jannik Neumann <jjmn@live.de>
2017-11-02 12:21:06 +0100

    Verbesserungen

--- GeldKonstrukor/Program.cs
+++ GeldKonstrukor/Program.cs
@@ -19,6 +19,7 @@ namespace GeldKonstrukor
         }
         public Konto(double betrag)     
         {
+            ktoNr = ktoGlobalNr++;
             ktoStand = betrag;      
         }
         public double getKtoStand()
@@ -38,9 +39,9 @@ namespace GeldKonstrukor
             Konto konto1 = new Konto();
             Konto konto2 = new Konto();
             Konto konto3 = new Konto();
-            Console.WriteLine("Das folgende Konto mit der Konto-Nummer: {0} wurde angelegt", konto1.getKtoNr());
-            Console.WriteLine("Das folgende Konto mit der Konto-Nummer: {0} wurde angelegt", konto2.getKtoNr());
-            Console.WriteLine("Das folgende Konto mit der Konto-Nummer: {0} wurde angelegt", konto3.getKtoNr());
+            Console.WriteLine("Das Konto {0} wurde angelegt.", konto1.getKtoNr());
+            Console.WriteLine("Das Konto {0} wurde angelegt.", konto2.getKtoNr());
+            Console.WriteLine("Das Konto {0} wurde angelegt.", konto3.getKtoNr());
             Console.ReadLine();
             
                 
