0c9a6dd2d21935431ed347ea5218f96fed57c4dc
Jannik Neumann <jjmn@live.de>
2017-11-25 00:35:33 +0100

    Rekursion erstellt, verbessert :D

---
+++ Rekursion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekursion
+{
+    class Rekursion
+    {
+        public String RekD(int x)
+        {
+            return RekA(x);
+        }
+
+        public String RekA(int n)
+        {
+            if (n <= 0)
+            {
+                return RekB();
+            }
+            else
+            {
+                return "Rickety " + RekA(n - 1);
+            }
+        }
+
+        public String RekB()
+        {
+            return "Wrecked";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rekursion TestRek = new Rekursion();
+            string test = TestRek.RekD(1);
+            Console.WriteLine(test);
+            Console.ReadLine();
+        }
+    }
+}
