e3f6412b7f3cc913c157af86023c00be542a4139
Jannik Neumann <jjmn@live.de>
2017-11-25 00:48:16 +0100

    "Rickety Wrecked!" (trans.: implemented methods from class Rekursion into the main method, removed mentioned class, cleared C.WL)

--- Rekursion/Program.cs
+++ Rekursion/Program.cs
@@ -6,38 +6,33 @@ using System.Threading.Tasks;
 
 namespace Rekursion
 {
-    class Rekursion
-    {
-        public String RekD(int x)
-        {
-            return RekA(x);
-        }
-
-        public String RekA(int n)
-        {
-            if (n <= 0)
-            {
-                return RekB();
-            }
-            else
-            {
-                return "Rickety " + RekA(n - 1);
-            }
-        }
-
-        public String RekB()
-        {
-            return "Wrecked";
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Rekursion TestRek = new Rekursion();
-            string test = TestRek.RekD(1);
-            Console.WriteLine(test);
+            String RekD(int x)
+            {
+                return RekA(x);
+            }
+
+            String RekA(int n)
+            {
+                if (n <= 0)
+                {
+                    return RekB();
+                }
+                else
+                {
+                    return "Rickety " + RekA(n - 1);
+                }
+            }
+
+            String RekB()
+            {
+                return "Wrecked";
+            }
+            
+            Console.WriteLine(RekD(1));
             Console.ReadLine();
         }
     }
