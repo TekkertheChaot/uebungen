c167fe45ffaa6a5da2a20cfc4e68d351621dcbe5
Jannik <jjmn@live.de>
2018-01-08 15:10:35 +0100

    Neue Projekte und Übungen

---
+++ TooManyDimensions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TooManyDimensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] myArray = new string[,]
+            {
+                {"1","2"},
+                {"3","4"},
+                {"5","6"}
+            };
+            Console.WriteLine(myArray[2,1]);
+        }
+    }
+}
---
+++ ZahlZerlegen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZahlZerlegen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int16 PIN;
+            Int16 PINx2;
+            Int16 qPIN;
+            Console.WriteLine("Gebe eine PIN ein!");
+            PIN = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Deine PIN lautet: {0}", PIN);
+            PINx2 = Convert.ToInt16(PIN * 2);
+            Console.WriteLine("Dein PIN mal 2 ist {0}", PINx2);
+            // -- Projekt Abgebrochen --
+        }
+    }
+}
